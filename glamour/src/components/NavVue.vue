76359c058bee003af7f3d709420535ac8d896f66
Alisher <alisherxujanov163@gmail.com>
2025-01-26 09:54:19 +0500

    installed more settings

--- glamour/src/components/NavVue.vue
+++ glamour/src/components/NavVue.vue
@@ -3,12 +3,54 @@
 </script>
 
 <template>
-  <div>
-    <h1>Nav</h1>
-  </div>
+    <nav>
+        <div class="top-nav">
+            <div class="left">
+                <a href="mailto:alisherxujanov163@gmail.com">
+                    alisherxujanov163@gmail.com
+                </a>
+                <a href="tel:+998334747477">
+                    +998 (33) 474-74-77
+                </a>
+            </div>
+            <div class="right">
+                <div class="nav-dropdown">
+                    English
+                </div>
+                <div class="nav-dropdown">
+                    USD
+                </div>
+                <a href="#">Login</a>
+                <a href="#">Wishlist</a>
+                <a href="#">🛒</a>
+            </div>
+        </div>
+        <div class="main-nav">
+            <div class="left">
+                <h2 id="logo">Glamour</h2>
+                <div class="nav-links">
+                    <a href="#">Home</a>
+                    <a href="#">Pages</a>
+                    <a href="#">Products</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Shop</a>
+                    <a href="#">Contact</a>
+                </div>
+            </div>
+            <div class="right">
+                <div class="searchbar">
+                    <input type="search">
+                    <button>🔍</button>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 
 <style lang="scss" scoped>
-
+nav {
+    .top-nav {}
+    .main-nav {}
+}
 </style>
