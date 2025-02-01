d602971300e20d90edba055f03464085aa990595
Alisher <alisherxujanov163@gmail.com>
2025-02-01 19:05:51 +0500

    update

--- glamour/src/views/ProductsView.vue
+++ glamour/src/views/ProductsView.vue
@@ -1,10 +1,15 @@
 <script setup>
 import HeadingVue from '@/components/HeadingVue.vue'
+import ShopItems from '@/components/products/shopList/ShopItems.vue'
+
+
 </script>
 
 <template>
   <div>
     <HeadingVue heading="Shop List" path="Home . Pages . Shop List" />
+
+    <ShopItems />
   </div>
 </template>
 
